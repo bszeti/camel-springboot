a5d842899abf5993dd37e40774bfb3f8c87ea9d2
Balazs Szeti <bszeti@localhost.localdomain>
2017-08-08 16:16:25 -0400

    Add swagger-ui

--- camel-rest-post/src/main/java/my/company/Application.java
+++ camel-rest-post/src/main/java/my/company/Application.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016 Red Hat, Inc.
- * <p>
- * Red Hat licenses this file to you under the Apache License, version
- * 2.0 (the "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.  See the License for the specific language governing
- * permissions and limitations under the License.
- *
- */
 package my.company;
 
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
@@ -22,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 // load regular Spring XML file from the classpath that contains the Camel XML DSL
@@ -35,11 +21,28 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * Register Camel into the web container
+     */
     @Bean
     ServletRegistrationBean servletRegistrationBean(){
     	ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/api/*");
     	servlet.setName("CamelServlet");
     	
     	return servlet;
+    }
+    
+    /**
+    * This is only a simple redirect to access swagger UI easier
+    * from "/swagger" to "/swagger-ui/index.html?url=/api/swagger&validatorUrl="
+    */
+    @Controller
+    class SwaggerWelcome {
+        @RequestMapping(
+            "/swagger"
+        )
+        public String redirectToUi() {
+            return "redirect:/swagger-ui/index.html?url=/api/swagger&validatorUrl=";
+        }
     }
 }
--- camel-rest-post/src/main/java/my/company/MyBuilder.java
+++ camel-rest-post/src/main/java/my/company/MyBuilder.java
@@ -2,22 +2,41 @@ package my.company;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyBuilder extends RouteBuilder {
+	private final static Logger log = LoggerFactory.getLogger(MyBuilder.class);
+	
+	@Value("${server.ssl.enabled: false}")
+	boolean sslEnabled;
+	
 	@Override
 	public void configure() throws Exception {
 		restConfiguration().component("servlet")
-		.contextPath("/api")
-		.bindingMode(RestBindingMode.json);
+		.bindingMode(RestBindingMode.json)
 		
-		rest("/")
+		//Swagger settings
+		.contextPath("/api") //base.path; use the mapping set for CamelServlet
+		.apiContextPath("/swagger") //swagger endpoint path; it will be under CamelServlet
+		.apiContextRouteId("swagger") //id of route providing the swagger endpoint
+		.apiProperty("api.title", "Example REST api")
+		.apiProperty("api.version", "1.0")
+		.apiProperty("schemes", sslEnabled ? "https" : "http" ) //Set https in swagger doc if ssl is enabled 
+		.apiProperty("host", "") //Setting empty string as host so swagger-ui make relative url calls. By default 0.0.0.0 is used
+		;
+		
+		rest()
 		.post("/user").type(UserPojo.class)
+			.responseMessage().code(200).endResponseMessage()
 			.route().routeId("post-user")
 			.log("User: ${body}")
 			.endRest()
 		.post("/country").type(CountryPojo.class)
+			.responseMessage().code(200).endResponseMessage()
 			.route().routeId("post-country")
 			.log("Country: ${body}")
 			.endRest()
