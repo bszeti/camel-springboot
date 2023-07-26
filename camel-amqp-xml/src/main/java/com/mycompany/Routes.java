789d221938c973160c972bf77eaf0e09f002c856
Balazs Szeti <szeti.balazs@gmail.com>
2023-07-26 12:28:07 -0400

    Add SQL

--- camel-amqp-xml/src/main/java/com/mycompany/Routes.java
+++ camel-amqp-xml/src/main/java/com/mycompany/Routes.java
@@ -18,13 +18,20 @@ public class Routes extends RouteBuilder {
          from("jms:queue:{{receive.queue}}?concurrentConsumers={{receive.concurrentConsumers}}&transacted=true&cacheLevelName=CACHE_CONSUMER")
                  .routeId("processXML")
              .log(LoggingLevel.INFO,"Received - ${body}")
+             // Validate XML
              .to("validator:xml/in.xsd")
 
-             .setHeader("OrderUUID",xpath("/order/@uuid"))
-                 .log("OrderUUID: ${header.OrderUUID}")
+             // Query by id
+             .setHeader("uuid",xpath("/order/@uuid",String.class))
+             .log("Order UUID: ${header.uuid}")
+             .to("sql:select * from orders where uuid=:#${header.uuid}?outputHeader=orderInfo&outputType=SelectOne")
+             // .to("log:debugexchange?showAll=true&multiline=true")
+             .log(LoggingLevel.INFO, "Query result : ${header.orderInfo[customer]} - ${header.orderInfo[address]}")
 
+             // XSLT
              .to("xslt:xml/in.xslt")
 
+             // Send message
              .log(LoggingLevel.INFO, "Sending - ${body}")
              .to("jms:queue:{{send.queue}}")
          ;
